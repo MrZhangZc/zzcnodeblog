d41ddb916ce95e66e57cef1690229804a3d37013
zzc_imau <jiayouzzc@126.com>
2017-09-15 01:37:26 +0800

    blog

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 
 
 var express = require('express'),
-  config = require('./config/config'),
-  glob = require('glob'),
-  mongoose = require('mongoose');
+    config = require('./config/config'),
+    glob = require('glob'),
+    mongoose = require('mongoose'),
+    mongoose.Promise = global.Promise;
+
+
 
 mongoose.connect(config.db);
 var db = mongoose.connection;
-db.on('error', function () {
-  throw new Error('unable to connect to database at ' + config.db);
-});
+	db.on('error', function () {
+	  throw new Error('unable to connect to database at ' + config.db);
+	});
 
 var models = glob.sync(config.root + '/app/models/*.js');
 models.forEach(function (model) {
--- config/config.js
+++ config/config.js
@@ -1,15 +1,16 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'production';
+    env = process.env.NODE_ENV || 'production',
+    mongoose.Promise = global.Promise;
 
 var config = {
   production: {
-    root: rootPath,
+    root: rootPath, 
     app: {
       name: 'blogzzc'
     },
     port: process.env.PORT || 5555,
-    db: 'mongodb://localhost/blogzzc-production'
+    db: 'mongodb://blog_runner:kobe241298@127.0.0.1:27017/blogzzc-production'
   }
 };
 
